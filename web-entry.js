907d252483cb3920cb4626feaf1f9d8461c882d3
webee.yw <webee.yw@gmail.com>
2021-01-08 18:22:40 +0800

    export USAKeyboardLayout

--- web-entry.js
+++ web-entry.js
@@ -1,8 +1,12 @@
 import puppeteer from 'puppeteer-core/lib/esm/puppeteer/web.js';
+import { keyDefinitions } from 'puppeteer-core/lib/esm/puppeteer/common/USKeyboardLayout.js';
+
+export const USAKeyboardLayout = { keyDefinitions };
 
 try {
   if (this === window) {
     window.puppeteer = puppeteer;
+    window.USAKeyboardLayout = USAKeyboardLayout;
   }
 } catch (err) {
   console.debug(err);
